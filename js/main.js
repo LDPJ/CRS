bc651ffbb87427b4efd04d4798f9c9e3f9f0415f
Nah.Myself( ) <larrypaulingjr@gmail.com>
2017-07-07 11:26:33 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,3 +23,72 @@ function errorCallback(error) {
 }
 
 navigator.getUserMedia(constraints, successCallback, errorCallback);
+
+
+pc1 = new RTCPeerConnection(servers);
+trace('Created local peer connection object pc1');
+pc1.onicecandidate = function(e) {
+  onIceCandidate(pc1, e);
+};
+
+pc1.addStream(localStream);
+
+function onIceCandidate(pc, event) {
+  if (event.candidate) {
+    getOtherPc(pc).addIceCandidate(
+      new RTCIceCandidate(event.candidate)
+    ).then(
+      function() {
+        onAddIceCandidateSuccess(pc);
+      },
+      function(err) {
+        onAddIceCandidateError(pc, err);
+      }
+    );
+    trace(getName(pc) + ' ICE candidate: \n' + event.candidate.candidate);
+  }
+}
+
+pc1.createOffer(
+    offerOptions
+  ).then(
+    onCreateOfferSuccess,
+    onCreateSessionDescriptionError
+  );
+
+function onCreateOfferSuccess(desc) {
+  pc1.setLocalDescription(desc).then(
+    function() {
+      onSetLocalSuccess(pc1);
+    },
+    onSetSessionDescriptionError
+  );
+  pc2.setRemoteDescription(desc).then(
+    function() {
+      onSetRemoteSuccess(pc2);
+    },
+    onSetSessionDescriptionError
+  );
+  // Since the 'remote' side has no media stream we need
+  // to pass in the right constraints in order for it to
+  // accept the incoming offer of audio and video.
+  pc2.createAnswer().then(
+    onCreateAnswerSuccess,
+    onCreateSessionDescriptionError
+  );
+}
+
+function onCreateAnswerSuccess(desc) {
+  pc2.setLocalDescription(desc).then(
+    function() {
+      onSetLocalSuccess(pc2);
+    },
+    onSetSessionDescriptionError
+  );
+  pc1.setRemoteDescription(desc).then(
+    function() {
+      onSetRemoteSuccess(pc1);
+    },
+    onSetSessionDescriptionError
+  );
+}
